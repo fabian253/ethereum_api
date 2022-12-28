26ee098f9c81b102ba8ed682414797a8e28f3de8
Fabian Galm <fabian.galm@gmail.com>
2022-12-28 17:32:41 +0100

    updated get_block method with query param block_number

--- app/main.py
+++ app/main.py
@@ -2,12 +2,16 @@ from fastapi import FastAPI
 import app.config as config
 from web3 import Web3
 from web3.beacon import Beacon
+from app.execution_client_connector import ExecutionClientConnector
 import json
 
 app = FastAPI()
 
-execution_client = execution_client = Web3(Web3.HTTPProvider(
-    f"http://{config.EXECUTION_CLIENT_IP}:{config.EXECUTION_CLIENT_PORT}"))
+#execution_client = Web3(Web3.HTTPProvider(
+#    f"http://{config.EXECUTION_CLIENT_IP}:{config.EXECUTION_CLIENT_PORT}"))
+
+execution_client = ExecutionClientConnector(
+    config.EXECUTION_CLIENT_IP, config.EXECUTION_CLIENT_PORT)
 
 consensus_client = Beacon(
     f"http://{config.CONSENCUS_CLIENT_IP}:{config.CONSENSUS_CLIENT_PORT}")
@@ -19,10 +23,13 @@ async def root():
 
 
 @app.get("/get_block")
-async def get_block():
-    response = execution_client.eth.get_block(16167991, True)
+async def get_block(block_number: int = None):
+    if block_number is not None:
+        response = execution_client.get_block(block_number)
+    else:
+        response = execution_client.get_block()
 
-    return json.loads(Web3.toJSON(response))
+    return response
 
 
 @app.get("/get_syncing")
