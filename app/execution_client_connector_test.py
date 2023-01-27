807f8c0fbafe502a9782977faf329f499805091e
Fabian Galm <fabian.galm@gmail.com>
2023-01-27 11:35:36 +0100

    update test

--- app/execution_client_connector_test.py
+++ app/execution_client_connector_test.py
@@ -6,7 +6,7 @@ execution_client = ExecutionClientConnector(
     config.EXECUTION_CLIENT_IP, config.EXECUTION_CLIENT_PORT)
 
 if __name__ == "__main__":
-    response = execution_client.get_transaction("0x09253105dd507588b6648b7dd07f0f97c532fc4de1817c9d943a81395f2ac892")
+    response = execution_client.get_syncing()
 
     json_object = json.dumps(response, indent=4)
 
