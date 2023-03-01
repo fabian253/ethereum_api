88ea3c428612cbff0fbc5fe20a3872d93b5853e5
Fabian Galm <fabian.galm@gmail.com>
2023-03-01 23:20:58 +0100

    added query parameters for consensus client

--- app/api_metadata/api_query_parameters.py
+++ app/api_metadata/api_query_parameters.py
@@ -35,3 +35,26 @@ TRANSACTION_HASH_QUERY_PARAMETER = Query(title="Transaction Hash", description="
 
 UNCLE_INDEX_QUERY_PARAMETER = Query(
     title="Uncle Index", description="Index of the uncle", example=0)
+
+STATE_ID_QUERY_PARAMETER = Query(
+    title="State ID",
+    description='State identifier. Can be one of: "head" (canonical head in node\'s view), "genesis", "finalized", "justified", <slot>, <hex encoded stateRoot with 0x prefix>.',
+    example="head")
+
+VALIDATOR_ID_QUERY_PARAMETER = Query(
+    title="Validator ID",
+    description="Either hex encoded public key (any bytes48 with 0x prefix) or validator index",
+    example=0
+)
+
+BLOCK_ID_QUERY_PARAMETER = Query(
+    title="Block ID",
+    description='Block identifier. Can be one of: "head" (canonical head in node\'s view), "genesis", "finalized", <slot>, <hex encoded blockRoot with 0x prefix>.',
+    example="head"
+)
+
+PEER_ID_QUERY_PARAMETER = Query(
+    title="Peer ID",
+    description="Peer identifier.",
+    example="16Uiu2HAm2m1M9H8JCi9krjdpHBiYDfntNbCgkJ5ha4qh3UsKDskh"
+)
