c32b0928c557bc26a4c08b22124cf1a3c13064a7
Fabian Galm <fabian.galm@gmail.com>
2023-03-01 13:02:31 +0100

    updated main with new response models and querys

--- app/main.py
+++ app/main.py
@@ -170,149 +170,230 @@ async def redoc_ui_html():
 
 # Execution Client Gossip Methods
 
-@app.get("/execution_client/block_number", tags=["Execution Client Gossip Methods"])
+@app.get("/execution_client/block_number", tags=["Execution Client Gossip Methods"], response_model=ExecutionClientResponseModelBlockNumber)
 async def execution_client_block_number(current_user: User = Depends(get_current_active_user)):
+    """
+    Returns the number of most recent block.
+    """
     response = execution_client.block_number()
     return response
 
 
 # Execution Client State Methods
 
-@app.get("/execution_client/default_account", tags=["Execution Client State Methods"])
+@app.get("/execution_client/default_account", tags=["Execution Client State Methods"], response_model=ExecutionClientResponseModelDefaultAccount)
 async def execution_client_default_account(current_user: User = Depends(get_current_active_user)):
+    """
+    Returns the ethereum address that will be used as the default from address for all transactions. 
+
+    Defaults to empty.
+    """
     response = execution_client.default_account()
     return response
 
 
-@app.get("/execution_client/default_block", tags=["Execution Client State Methods"])
+@app.get("/execution_client/default_block", tags=["Execution Client State Methods"], response_model=ExecutionClientResponseModelDefaultBlock)
 async def execution_client_default_block(current_user: User = Depends(get_current_active_user)):
+    """
+    Returns the default block number that will be used for any RPC methods that accept a block identifier. 
+
+    Defaults to 'latest'.
+    """
     response = execution_client.default_block()
     return response
 
 
-@app.get("/execution_client/syncing", tags=["Execution Client State Methods"])
+@app.get("/execution_client/syncing", tags=["Execution Client State Methods"], response_model=ExecutionClientResponseModelSyncing)
 async def execution_client_syncing(current_user: User = Depends(get_current_active_user)):
+    """
+    Returns either False if the node is not syncing or a dictionary showing sync status.
+    """
+    # TODO: include second response model if node is not syncing
     response = execution_client.syncing()
     return response
 
 
-@app.get("/execution_client/mining", tags=["Execution Client State Methods"])
+@app.get("/execution_client/mining", tags=["Execution Client State Methods"], response_model=ExecutionClientResponseModelMining)
 async def execution_client_mining(current_user: User = Depends(get_current_active_user)):
+    """
+    Returns boolean as to whether the node is currently mining.
+    """
     response = execution_client.mining()
     return response
 
 
-@app.get("/execution_client/hashrate", tags=["Execution Client State Methods"])
+@app.get("/execution_client/hashrate", tags=["Execution Client State Methods"], response_model=ExecutionClientResponseModelHashrate)
 async def execution_client_hashrate(current_user: User = Depends(get_current_active_user)):
+    """
+    Returns the current number of hashes per second the node is mining with.
+    """
     response = execution_client.hashrate()
     return response
 
 
-@app.get("/execution_client/max_priority_fee", tags=["Execution Client State Methods"])
+@app.get("/execution_client/max_priority_fee", tags=["Execution Client State Methods"], response_model=ExecutionClientResponseModelMaxPriorityFee)
 async def execution_client_max_priority_fee(current_user: User = Depends(get_current_active_user)):
+    """
+    Returns a suggestion for a max priority fee for dynamic fee transactions in Wei.
+    """
     response = execution_client.max_priority_fee()
     return response
 
 
-@app.get("/execution_client/accounts", tags=["Execution Client State Methods"])
+@app.get("/execution_client/accounts", tags=["Execution Client State Methods"], response_model=ExecutionClientResponseModelAccounts)
 async def execution_client_accounts(current_user: User = Depends(get_current_active_user)):
+    """
+    Returns the list of known accounts.
+    """
     response = execution_client.accounts()
     return response
 
 
-@app.get("/execution_client/chain_id", tags=["Execution Client State Methods"])
+@app.get("/execution_client/chain_id", tags=["Execution Client State Methods"], response_model=ExecutionClientResponseModelChainId)
 async def execution_client_chain_id(current_user: User = Depends(get_current_active_user)):
+    """
+    Returns an integer value for the currently configured “Chain Id” value introduced in EIP-155.
+
+    Returns None if no Chain Id is available.
+    """
+    # TODO: add None into response model
     response = execution_client.chain_id()
 
     return response
 
 
-@app.get("/execution_client/get_api_version", tags=["Execution Client State Methods"])
+@app.get("/execution_client/get_api_version", tags=["Execution Client State Methods"], response_model=ExecutionClientResponseModelGetApiVersion)
 async def execution_client_get_api_version(current_user: User = Depends(get_current_active_user)):
+    """
+    Returns the id of the current API version.
+    """
     response = execution_client.get_api_version()
     return response
 
 
-@app.get("/execution_client/get_client_version", tags=["Execution Client State Methods"])
+@app.get("/execution_client/get_client_version", tags=["Execution Client State Methods"], response_model=ExecutionClientResponseModelGetClientVersion)
 async def execution_client_get_client_version(current_user: User = Depends(get_current_active_user)):
+    """
+    Returns the id of the current Ethereum protocol version.
+    """
     response = execution_client.get_client_version()
     return response
 
 
-@app.get("/execution_client/get_balance", tags=["Execution Client State Methods"], response_model=ExecutionClientGetBalanceResponseModel)
+@app.get("/execution_client/get_balance", tags=["Execution Client State Methods"], response_model=ExecutionClientResponseModelGetBalance)
 async def execution_client_get_balance(
         wallet_address: str = WALLET_ADDRESS_QUERY_PARAMETER,
-        block_identifier: Union[int, None] = BLOCK_IDENTIFIER_QUERY_PARAMETER,
+        block_identifier: Union[int, str,
+                                None] = BLOCK_IDENTIFIER_OPTIONAL_QUERY_PARAMETER,
         current_user: User = Depends(get_current_active_user)):
     """
-    Returns the balance of the account of given address.
+    Returns the balance of the given wallet_address at the block specified by block_identifier.
+
+    wallet_address may be a checksum address or an ENS name
     """
     response = execution_client.get_balance(wallet_address, block_identifier)
     return response
 
 
-@app.get("/execution_client/get_block_number", tags=["Execution Client State Methods"])
+@app.get("/execution_client/get_block_number", tags=["Execution Client State Methods"], response_model=ExecutionClientResponseModelGetBlockNumber)
 async def execution_client_get_block_number(current_user: User = Depends(get_current_active_user)):
+    """
+    Returns the number of the most recent block.
+    """
     response = execution_client.get_block_number()
     return response
 
 
-@app.get("/execution_client/get_storage_at", tags=["Execution Client State Methods"], response_model=ExecutionClientGetStorageAtResponseModel)
+@app.get("/execution_client/get_storage_at", tags=["Execution Client State Methods"], response_model=ExecutionClientResponseModelGetStorageAt)
 async def execution_client_get_storage_at(
         wallet_address: str = WALLET_ADDRESS_QUERY_PARAMETER,
         position: int = POSITION_QUERY_PARAMETER,
-        block_identifier: Union[int, None] = BLOCK_IDENTIFIER_QUERY_PARAMETER,
+        block_identifier: Union[int, str,
+                                None] = BLOCK_IDENTIFIER_OPTIONAL_QUERY_PARAMETER,
         current_user: User = Depends(get_current_active_user)):
     """
-    Returns the value from a storage position at a given address.
+    Returns the value from a storage position for the given wallet_address at the block specified by block_identifier.
+
+    wallet_address may be a checksum address or an ENS name
     """
     response = execution_client.get_storage_at(
         wallet_address, position, block_identifier)
     return response
 
 
-@app.get("/execution_client/get_code", tags=["Execution Client State Methods"])
+@app.get("/execution_client/get_code", tags=["Execution Client State Methods"], response_model=ExecutionClientResponseModelGetCode)
 async def execution_client_get_code(
-        wallet_address: str,
+        wallet_address: str = WALLET_ADDRESS_QUERY_PARAMETER,
         current_user: User = Depends(get_current_active_user)):
+    """
+    Returns the bytecode for the given wallet_address at the block specified by block_identifier.
+
+    wallet_address may be a checksum address or an ENS name    
+    """
     response = execution_client.get_code(wallet_address)
     return response
 
 
-@app.get("/execution_client/get_transaction_count", tags=["Execution Client State Methods"])
+@app.get("/execution_client/get_transaction_count", tags=["Execution Client State Methods"], response_model=ExecutionClientResponseModelGetTransactionCount)
 async def execution_client_get_transaction_count(
-        wallet_address: str,
-        block_identifier: Union[int, None] = None,
+        wallet_address: str = WALLET_ADDRESS_QUERY_PARAMETER,
+        block_identifier: Union[int, str,
+                                None] = BLOCK_IDENTIFIER_OPTIONAL_QUERY_PARAMETER,
         current_user: User = Depends(get_current_active_user)):
+    """
+    Returns the number of transactions that have been sent from wallet_address as of the block specified by block_identifier.
+
+    wallet_address may be a checksum address or an ENS name
+    """
     reponse = execution_client.get_transaction_count(
         wallet_address, block_identifier)
     return reponse
 
 
-@app.get("/execution_client/estimate_gas", tags=["Execution Client State Methods"])
+@app.get("/execution_client/estimate_gas", tags=["Execution Client State Methods"], response_model=ExecutionClientResponseModelEstimateGas)
 async def execution_client_estimate_gas(
-        from_address: str,
-        to_address: str,
-        value: int,
+        from_address: str = FROM_ADDRESS_QUERY_PARAMETER,
+        to_address: str = TO_ADDRESS_QUERY_PARAMETER,
+        value: int = TRANSACTION_VALUE_QUERY_PARAMETER,
         current_user: User = Depends(get_current_active_user)):
+    """
+    Executes the given transaction locally without creating a new transaction on the blockchain. Returns amount of gas consumed by execution which can be used as a gas estimate.
+
+    The transaction and block_identifier parameters are handled in the same manner as the send_transaction() method.
+    """
+    # TODO: not working -> node not fully synced
     response = execution_client.estimate_gas(from_address, to_address, value)
     return response
 
 
 # Execution Client History Methods
 
-@app.get("/execution_client/get_block_transaction_count", tags=["Execution Client History Methods"])
+@app.get("/execution_client/get_block_transaction_count", tags=["Execution Client History Methods"], response_model=ExecutionClientResponseModelGetBlockTransactionCount)
 async def execution_client_get_block_transaction_count(
-        block_identifier: Union[int, str],
+        block_identifier: Union[int, str] = BLOCK_IDENTIFIER_QUERY_PARAMETER,
         current_user: User = Depends(get_current_active_user)):
+    """
+    Returns the number of transactions in the block specified by block_identifier. 
+    Delegates to eth_getBlockTransactionCountByNumber if block_identifier is an integer or one of the predefined block parameters 'latest', 'earliest', 'pending', 'safe', 'finalized', otherwise delegates to eth_getBlockTransactionCountByHash. 
+
+    Throws BlockNotFoundError if transactions are not found.
+    """
+    # TODO: add error for block not found
     response = execution_client.get_block_transaction_count(block_identifier)
     return response
 
 
-@app.get("/execution_client/get_uncle_count", tags=["Execution Client History Methods"])
+@app.get("/execution_client/get_uncle_count", tags=["Execution Client History Methods"], response_model=ExecutionClientResponseModelGetUncleCount)
 async def execution_client_get_uncle_count(
-        block_identifier: Union[int, str],
+        block_identifier: Union[int,
+                                str] = BLOCK_IDENTIFIER_UNCLE_QUERY_PARAMETER,
         current_user: User = Depends(get_current_active_user)):
+    """
+    Returns the (integer) number of uncles associated with the block specified by block_identifier. 
+    Delegates to eth_getUncleCountByBlockNumber if block_identifier is an integer or one of the predefined block parameters 'latest', 'earliest', 'pending', otherwise delegates to eth_getUncleCountByBlockHash. 
+
+    Throws BlockNotFound if the block is not found.
+    """
     response = execution_client.get_uncle_count(block_identifier)
 
     if response is None:
@@ -324,12 +405,23 @@ async def execution_client_get_uncle_count(
     return response
 
 
-@app.get("/execution_client/get_block", tags=["Execution Client History Methods"])
+@app.get("/execution_client/get_block", tags=["Execution Client History Methods"], response_model=Union[ExecutionClientResponseModelGetBlockFalse, ExecutionClientResponseModelGetBlockTrue])
 async def execution_client_get_block(
-        block_identifier: Union[int, str, None] = None,
-        full_transactions: bool = False,
+        block_identifier: Union[int, str,
+                                None] = BLOCK_IDENTIFIER_OPTIONAL_QUERY_PARAMETER,
+        full_transactions: bool = FULL_TRANSACTION_QUERY_PARAMETER,
         current_user: User = Depends(get_current_active_user)):
+    """
+    Returns the block specified by block_identifier. 
+    Delegates to eth_getBlockByNumber if block_identifier is an integer or one of the predefined block parameters 'latest', 'earliest', 'pending', 'safe', 'finalized' - otherwise delegates to eth_getBlockByHash. 
+
+    Throws BlockNotFound error if the block is not found.
+
+    If full_transactions is True then the 'transactions' key will contain full transactions objects. Otherwise it will be an array of transaction hashes.
+    """
     response = execution_client.get_block(block_identifier, full_transactions)
+
+    print(response)
 
     if response is None:
         raise HTTPException(
@@ -340,11 +432,17 @@ async def execution_client_get_block(
     return response
 
 
-@app.get("/execution_client/get_transaction_by_block", tags=["Execution Client History Methods"])
+@app.get("/execution_client/get_transaction_by_block", tags=["Execution Client History Methods"], response_model=ExecutionClientResponseModelGetTransactionByBlock)
 async def execution_client_get_transaction_by_block(
-        block_identifier: Union[int, str],
-        transaction_index: int,
+        block_identifier: Union[int, str] = BLOCK_IDENTIFIER_QUERY_PARAMETER,
+        transaction_index: int = TRANSACTION_INDEX_QUERY_PARAMETER,
         current_user: User = Depends(get_current_active_user)):
+    """
+    Returns the transaction at the index specified by transaction_index from the block specified by block_identifier. 
+    Delegates to eth_getTransactionByBlockNumberAndIndex if block_identifier is an integer or one of the predefined block parameters 'latest', 'earliest', 'pending', 'safe', 'finalized', otherwise delegates to eth_getTransactionByBlockHashAndIndex. 
+
+    Throws TransactionNotFound if a transaction is not found at specified arguments.
+    """
     response = execution_client.get_transaction_by_block(
         block_identifier, transaction_index)
 
@@ -357,19 +455,34 @@ async def execution_client_get_transaction_by_block(
     return response
 
 
-@app.get("/execution_client/get_transaction_receipt", tags=["Execution Client History Methods"])
+@app.get("/execution_client/get_transaction_receipt", tags=["Execution Client History Methods"], response_model=ExecutionClientResponseModelGetTransactionReceipt)
 async def execution_client_get_transaction_receipt(
-        transaction_hash: str,
+        transaction_hash: str = TRANSACTION_HASH_QUERY_PARAMETER,
         current_user: User = Depends(get_current_active_user)):
+    """
+    Returns the transaction receipt specified by transaction_hash. 
+
+    Throws TransactionNotFound if a transaction cannot be found.
+
+    If status in response equals 1 the transaction was successful. If it is equals 0 the transaction was reverted by EVM.
+    """
+    # TODO: Add exception
     response = execution_client.get_transaction_receipt(transaction_hash)
     return response
 
 
-@app.get("/execution_client/get_uncle_by_block", tags=["Execution Client History Methods"])
+@app.get("/execution_client/get_uncle_by_block", tags=["Execution Client History Methods"], response_model=ExecutionClientResponseModelGetUncleByBlock)
 async def execution_client_get_uncle_by_block(
-        block_identifier: Union[int, str],
-        uncle_index: int,
+        block_identifier: Union[int,
+                                str] = BLOCK_IDENTIFIER_UNCLE_QUERY_PARAMETER,
+        uncle_index: int = UNCLE_INDEX_QUERY_PARAMETER,
         current_user: User = Depends(get_current_active_user)):
+    """
+    Returns the uncle at the index specified by uncle_index from the block specified by block_identifier. 
+    Delegates to eth_getUncleByBlockNumberAndIndex if block_identifier is an integer or one of the predefined block parameters 'latest', 'earliest', 'pending', otherwise delegates to eth_getUncleByBlockHashAndIndex. 
+
+    Throws BlockNotFound if the block is not found.
+    """
     response = execution_client.get_uncle_by_block(
         block_identifier, uncle_index)
 
@@ -413,13 +526,12 @@ async def consensus_client_get_finality_checkpoint(
     response = consensus_client.get_finality_checkpoint(state_id)
     return response
 
-# TODO: not working
-
 
 @app.get("/consensus_client/get_validators", tags=["Consensus Client Beacon Methods"])
 async def consensus_client_get_validators(
         state_id: str = "head",
         current_user: User = Depends(get_current_active_user)):
+    # TODO: not working
     response = consensus_client.get_validators(state_id)
     return response
 
